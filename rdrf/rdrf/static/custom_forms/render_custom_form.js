63c22665b36077b9abd445b8b360bb9814c42e62
samuel kyama <samuelkyama102@gmail.com>
2025-02-03 05:55:28 +0300

    update render_custom_form.js to use external IP address for FORMIO_URL

--- rdrf/rdrf/static/custom_forms/render_custom_form.js
+++ rdrf/rdrf/static/custom_forms/render_custom_form.js
@@ -161,7 +161,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const formName = formContainer.dataset.formName;
   const patientId = formContainer.dataset.patientId;
   const registryId = formContainer.dataset.registryId;
-  // const formioUrl = formContainer.dataset.formioUrl || "http://localhost:3001";
+  // const formioUrl = formContainer.dataset.formioUrl || "http://67.205.168.170:3001";
   const formioUrl = "http://67.205.168.170:3001";
   renderFormioForm(formName, patientId, registryId, formioUrl);
 });
